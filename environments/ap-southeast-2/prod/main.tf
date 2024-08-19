ce8fbf33853c012d55363bf5376533c462dad130
Dzenad Custovic <dzenad.custovic@hotmail.com>
2024-08-19 10:39:26 +0200

    removing nat gateway

--- environments/ap-southeast-2/prod/main.tf
+++ environments/ap-southeast-2/prod/main.tf
@@ -14,9 +14,9 @@ module "vpc" {
   private_subnets = ["10.6.1.0/24", "10.6.2.0/24", "10.6.3.0/24"]
   public_subnets  = ["10.6.101.0/24", "10.6.102.0/24", "10.6.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = false
-  one_nat_gateway_per_az = true
+  one_nat_gateway_per_az = false
 
   enable_vpn_gateway = false
 
